673d0a20ccc5a495ac3280e5d3ee3f425ad85168
peachk3 <doo062991@gmail.com>
2024-07-16 19:59:19 +0900

    #VO 수정 완료

--- Gaebar2zo/src/main/java/com/itwillbs/domain/WarehouseCodeVO.java
+++ Gaebar2zo/src/main/java/com/itwillbs/domain/WarehouseCodeVO.java
@@ -7,6 +7,10 @@ public class WarehouseCodeVO {
 
 	private	String	s_cate_wh_code;
 	private	String	s_cate_wh_name;
-	private	String	m_cate_wh_code;
+	private	String	wh_tel;
+	private	String	wh_add1;
+	private	String	wh_add2;
+	private	String	wh_man;
+	private	Integer	wh_status;
 	
 }
--- Gaebar2zo/src/main/java/com/itwillbs/domain/WarehouseVO.java
+++ Gaebar2zo/src/main/java/com/itwillbs/domain/WarehouseVO.java
@@ -7,11 +7,10 @@ public class WarehouseVO {
 	
 	private	String	wh_num;
 	private	String	wh_name;
-	private	String	wh_tel;
-	private	String	wh_add1;
-	private	String	wh_add2;
-	private	String	wh_man;
-	private	Boolean	wh_status;
+	private String 	wh_zone;
+	private String  wh_rack;
+	private String  wh_row;
+	private String  wh_column;
 	private	String	wh_code;
 
 }

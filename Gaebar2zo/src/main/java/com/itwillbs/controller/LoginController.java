804b0c2afbdbdab591ec7976996709d951b16f96
peachk3 <163658255+peachk3@users.noreply.github.com>
2024-08-08 09:57:06 +0900

    Update LoginController.java

--- Gaebar2zo/src/main/java/com/itwillbs/controller/LoginController.java
+++ Gaebar2zo/src/main/java/com/itwillbs/controller/LoginController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
    @Controller
    public class LoginController {
 
-       @GetMapping("/loginout/login")
+       @GetMapping("/Styleboso/loginout/login")
        public String loginPage() {
            return "/Styleboso/loginout/login";
        }
 
-       @GetMapping("/loginout/main")
+       @GetMapping("/Styleboso/loginout/main")
        public String mainPage() {
            return "/Styleboso/loginout/main";
        }
        
-        @GetMapping("/loginout/accessErr")
+        @GetMapping("/Styleboso/loginout/accessErr")
            public String accessDenied() {
                return "/loginout/accessErr";
            }
